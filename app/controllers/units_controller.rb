5cebdd73837a4d21703aaa166ae8b955c1077e4d
cedricdlb <cdlb.git@gmail.com>
2016-09-25 20:13:31 -0700

    - Expand functionality to handle parentheses groups  - Extract main loop functionality into a function which can be called    recursively when an open parentheses opens a new group.  - Clean up and simplify code  - Handle pluralized inputs and unrecognized input.  - Add rest of rails files that were added in prior commit.

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,16 +1,5 @@
 class UnitsController < ApplicationController
-  SI_UNIT_CONVERSIONS = [
-    {unit_regexp: /minute|min/, type: "time",        unit_name: "s",   multiplication_factor: 60.0 },
-    {unit_regexp: /hour|h/,     type: "time",        unit_name: "s",   multiplication_factor: 3600.0 },
-    {unit_regexp: /day|d/,      type: "time",        unit_name: "s",   multiplication_factor: 86400.0 },
-    {unit_regexp: /degree|°/,   type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 180.0) },
-    {unit_regexp: /‘/,          type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 10800.0) },
-    {unit_regexp: /second|“/,   type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 648000.0) },
-    {unit_regexp: /hectare|ha/, type: "area",        unit_name: "m²",  multiplication_factor: 10000.0 },
-    {unit_regexp: /litre|L/,    type: "volume",      unit_name: "m³",  multiplication_factor: 0.001 },
-    {unit_regexp: /tonne|t/,    type: "mass",        unit_name: "kg",  multiplication_factor: 1000.0 },
-  ]
-  SI_UNIT_CONVERSIONS_HASH = {
+  SI_UNIT_CONVERSIONS = {
     "minute"  => { type: "time",        unit_name: "s",   multiplication_factor: 60.0 },
     "min"     => { type: "time",        unit_name: "s",   multiplication_factor: 60.0 },
     "hour"    => { type: "time",        unit_name: "s",   multiplication_factor: 3600.0 },
@@ -18,7 +7,8 @@ class UnitsController < ApplicationController
     "day"     => { type: "time",        unit_name: "s",   multiplication_factor: 86400.0 },
     "d"       => { type: "time",        unit_name: "s",   multiplication_factor: 86400.0 },
     "degree"  => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 180.0) },
-    "°"       => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 180.0) },
+    "°"       => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 180.0) }, # ° is Shift-Option 8 in vim, same as spec
+    "˚"       => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 180.0) }, # ˚ is Option k in vim, different from spec
     "‘"       => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 10800.0) },
     "second"  => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 648000.0) },
     "“"       => { type: "Plane angle", unit_name: "rad", multiplication_factor: (Math::PI / 648000.0) },
@@ -29,61 +19,51 @@ class UnitsController < ApplicationController
     "tonne"   => { type: "mass",        unit_name: "kg",  multiplication_factor: 1000.0 },
     "t"       => { type: "mass",        unit_name: "kg",  multiplication_factor: 1000.0 },
   }
-  DELIMETERS = %r{([*/()])}
+  OPERATORS = %r{([*/()])}
   
-  MULTIPLY = Proc.new { |x, y|  x * y }
-  DIVIDE   = Proc.new { |x, y|  x / y }
-
   def si
-    units = params["units"]
-   #tokens = units.split(%r{([*/()])}).reject(&:blank?)
-    tokens = units.split(DELIMETERS).reject(&:blank?)
+    units = params["units"] || ""
+    tokens = units.split(OPERATORS).reject(&:blank?).map(&:singularize)
+    # Above, I reject blanks from split's returned array because
+    # if there are two adjacent operators in the string,
+    # split infers an empty string between them for the returned array,
+    #puts "in units_controller#si tokens #{tokens}"
 
-    puts "in units_controller#si params #{params}"
-    puts "in units_controller#si units #{units}"
-    puts "in units_controller#si tokens #{tokens}"
-    
-    @unit_name = ""
-    @factor = 1
-    next_operation = MULTIPLY
-    
-    tokens.each do |token|
-      # TODO Watch for pluralized units
-      conversion = SI_UNIT_CONVERSIONS_HASH[token]
-      if conversion
-        @factor = next_operation.call(@factor, conversion[:multiplication_factor])
-        @unit_name += conversion[:unit_name]
-      else # must be a delimeter
-        case token
-        when %r{[/]}
-          next_operation = DIVIDE
-        when %r{[*]}
-          next_operation = MULTIPLY
-    #   when %r{[(]}
-    #     next_operation = open_group
-    #   when %r{[)]}
-    #     next_operation = close_group
-        end
-        @unit_name += token # TODO be Careful of gibberish input tokens
-      end
-
-    end
-
-
-  # if units =~ /minute/
-  #   @unit_name += "s"
-  #   @factor *= 60.0
-  # end
+    @factor, @unit_name, @errors = collect_unit_names_and_factor(tokens)
     @factor = @factor.round(14)
-    render "units/si.json.jbuilder", unit_name: @unit_name, factor: @factor
+    if @errors.empty?
+      render "units/si.json.jbuilder", unit_name: @unit_name, factor: @factor
+    else
+      @errors << "Allowed operators are [(, ), *, /] Allowed units are: [#{SI_UNIT_CONVERSIONS.keys.join(", ")}]"
+      render :json => { :errors => @errors }, :status => 400
+    end
   end
 
+  private
+  def collect_unit_names_and_factor(tokens, unit_name = "", errors = [])  
+    factor = 1.0
+    multipy_or_divide_by = :*
+    while(token = tokens.shift)
+      conversion = SI_UNIT_CONVERSIONS[token]
+      if conversion
+        factor = factor.send(multipy_or_divide_by, conversion[:multiplication_factor])
+        unit_name += conversion[:unit_name]
+      else # must be an operator
+        unit_name += token
+        case token
+        when /[*\/]/
+          multipy_or_divide_by = token.to_sym
+        when /[(]/
+          sub_factor, unit_name, errors = collect_unit_names_and_factor(tokens, unit_name, errors)
+          factor = factor.send(multipy_or_divide_by, sub_factor)
+        when /[)]/
+          return [factor, unit_name, errors]
+        else
+          errors << "Unrecognized token: #{token}."
+        end
+      end
+    end
+    return [factor, unit_name, errors]
+  end
 
-# def multiply(x, y)
-#   x*y
-# end
-
-# def devide(x, y)
-#   x/y
-# end
 end
--- test/controllers/units_controller_test.rb
+++ test/controllers/units_controller_test.rb
@@ -1,16 +1,18 @@
 require 'test_helper'
 
 class UnitsControllerTest < ActionController::TestCase
-#  test "should get si" do
-#    get :si
-#    assert_response :success
-#  end
+   test "should get si" do
+     get :si
+     assert_response :success
+   end
     
-#  test "should get si with params foo" do
-#    get :si
-#    json = JSON.parse(response.body)
-#    assert_equal("bar", json["foo"], "Unexpected value for json['foo'] (Expected bar)")
-#  end
+ #test "should get si with params minute and return s and factor" do
+  test "minute" do
+    get :si, format: :json, units: "minute"
+    json = JSON.parse(response.body)
+    assert_equal({ "unit_name" => "s", "multiplication_factor" => "60.0" }, json, 
+                 'Unexpected value for json (Expected { "unit_name" => "s", "multiplication_factor" => "60.0" })')
+  end
 
   # Here is an example input and output for “degrees/minute”:
   # GET "/units/si?units=degrees/minute"
@@ -23,13 +25,27 @@ class UnitsControllerTest < ActionController::TestCase
                  'Unexpected value for json (Expected { "unit_name" => "rad/s", "multiplication_factor" => "0.00029088820867" })')
   end
  
- #test "should get si with params minute and return s and factor" do
-  test "minute" do
-    get :si, format: :json, units: "minute"
+  test "degrees_minute_tonne" do
+    get :si, format: :json, units: "(degrees*minute)/(tonne)"
     json = JSON.parse(response.body)
-    assert_equal({ "unit_name" => "s", "multiplication_factor" => "60.0" }, json, 
-                 "Unexpected value for json (Expected { \"unit_name\" => \"s\", \"multiplication_factor\" => \"60.0\" })")
+    assert_equal({ "unit_name" => "(rad*s)/(kg)", "multiplication_factor" => "0.0010471975512" }, json,
+                 'Unexpected value for json (Expected { "unit_name" => "(rad*s)/(kg)", "multiplication_factor" => "0.0010471975512" })')
   end
  
-
+  test "its_a_complicated_one" do
+    get :si, format: :json, units: "(minutes/min)*(hs/hour)*(days/d)*(°/˚)*(°/degrees)*(‘/‘)*(“/second)*(ha/hectare)*(litre/L)*(t/tonnes)"
+    json = JSON.parse(response.body)
+    assert_equal({ "unit_name" => "(s/s)*(s/s)*(s/s)*(rad/rad)*(rad/rad)*(rad/rad)*(rad/rad)*(m²/m²)*(m³/m³)*(kg/kg)", "multiplication_factor" => "1.0" }, json,
+                 'Unexpected value for json (Expected all factors to cancel out to 1.0)')
+  end
+ 
+  test "errored_input" do
+    get :si, format: :json, units: "(degrees*minute/cats)/(tonne*dogs)"
+    json = JSON.parse(response.body)
+    assert_response(:bad_request, "Expected an client error status code 400, Bad Request")
+    assert_includes( json["errors"], "Unrecognized token: dog.", "Expected an error about a bad dog" )
+    assert_includes( json["errors"], "Unrecognized token: cat.", "Expected an error about a bad cat" )
+    allowed = 'Allowed operators are [(, ), *, /] Allowed units are: [minute, min, hour, h, day, d, degree, °, ˚, ‘, second, “, hectare, ha, litre, L, tonne, t]'
+    assert_includes( json["errors"], allowed, "Expected errors to indicate allowed inputs" )
+  end
 end
